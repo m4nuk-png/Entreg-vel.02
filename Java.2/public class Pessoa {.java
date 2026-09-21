5c304dabb99fa00de8f58a07eec8e019c43a5493
Manu-png <emanuellamendes41@gmail.com>
2026-09-20 22:35:38 -0300

    entregavel.02

---
+++ Java.2/public class Pessoa {.java
@@ -0,0 +1,14 @@
+public class Pessoa {
+
+    // Atributos da classe Pessoa
+    String nome;
+    int idade;
+    String profissao;
+
+    // Construtor
+    public Pessoa(String nome, int idade, String profissao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.profissao = profissao;
+    }
+}
